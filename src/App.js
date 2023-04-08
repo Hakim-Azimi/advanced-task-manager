8fe398012c322e409094c51af18a68345d63f429
Hakim-Azimi <129004867+Hakim-Azimi@users.noreply.github.com>
2023-04-08 02:45:33 -0700

    handeling doneHandler and dynamic done button

--- src/App.js
+++ src/App.js
@@ -7,28 +7,48 @@ import Header from './components/header/Header';
 function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+  const today = new Date();
+const day = today.getDate();
+const month = today.getMonth() + 1; // در جاوا اسکریپت، ماه‌ها از 0 شروع می‌شوند
+const year = today.getFullYear();
+const todayString = day + "/" + month + "/" + year;
  
   
-  const submitHandler = (e)=>{
-    e.preventDefault();
-    setTasks([...tasks, {title: task, id: Date.now()}]);
-    setTask("");
-  }
+  
   const changeHandler = (e)=>{
 setTask(e.target.value);
   }
   // handle above removeHandler
  const removeHandler = (id)=>{
-  console.log(id)
-  const filteredTasks = tasks.filter(t=> t.id !== task.id);
-  alert(filteredTasks);
-  setTasks(filteredTasks);
+  if(window.confirm){
+    const filteredTasks = tasks.filter((t)=> t.id !== id);
+    setTasks(filteredTasks);
+  }
  }
+ const submitHandler = (e)=>{
+  e.preventDefault();
+  setTasks([...tasks, {title: task,done: false,created: todayString, id: Date.now()}]);
+  setTask("");
+}
+const doneHandler = (id)=>{
+  const index = tasks.findIndex((item)=> item.id === id);
+  const updatedTasks = [...tasks];
+  updatedTasks[index] = {
+    id: tasks[index].id,
+    title: tasks[index].title,
+    done: !tasks[index].done,
+    created:tasks[index].created,
+    finished: tasks[index].todayString,
+
+  }
+  setTasks(updatedTasks);
+  console.log(updatedTasks)
+}
   return (
     <div className="App">
       <Header/>
       <NewTodo id={task.id}  change={changeHandler} task={task} submit={submitHandler}/>
-    <TodoList remove={removeHandler} title={task} tasks={tasks} />
+    <TodoList  completed={doneHandler} time={todayString} id={task.id} remove={removeHandler} title={task} tasks={tasks} />
     </div>
   );
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,28 +1,28 @@
-import './list.css';
-import { useEffect } from 'react';
-const Todolist = ({tasks, remove}) => {
+import "./list.css";
+import { useEffect } from "react";
+const Todolist = ({completed, time, tasks, remove }) => {
   useEffect(() => {
     console.log(tasks);
   }, [tasks]);
 
-  return ( 
-    <div className='lists'>
-
-      {tasks && tasks.map((todo)=>{
-        return (
-          <div className='list'>
-           <div className='list-title'>{todo.title}</div>
-            <div className='list-btns'>
+  return (
+    <div className="lists">
+      {tasks &&
+        tasks.map((todo) => {
+          return (
+            <div className="list" key={todo.id}>
+              <div className="list-title">{todo.title}</div>
+              created in {time}
               
-              <button>edit </button>
-              <button onClick={remove}>remove </button>
+              <div className="list-btns">
+                <button onClick={() => completed(todo.id)}>{todo.done? "undo": "done"} </button>
+                <button onClick={() => remove(todo.id)}>remove </button>
+              </div>
             </div>
-          </div>
-        )
-      })}
-
+          );
+        })}
     </div>
-   );
-}
+  );
+};
 
 export default Todolist;
